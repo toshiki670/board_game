380826d39b9054cd6c8dab43cd693f235fa143a3
toshiki <toshiki@Archlinux.localdomain>
2019-07-15 23:53:58 +0900

    Add ユーザー変更

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -1,8 +1,19 @@
 package model.player;
 
 public interface Player {
-  
+
   public String getUserName();
+
   public void setUserName(String userName);
-  
+
+  public static Player changer(Player player) {
+    if (player == DarkPlayer.getInstance()) {
+      return WhitePlayer.getInstance();
+    }
+    if (player == WhitePlayer.getInstance()) {
+      return DarkPlayer.getInstance();
+    }
+    return player;
+  }
+
 }
