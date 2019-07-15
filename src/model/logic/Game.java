abcaf8bcd5d92d29bcb63328e45d60e5b40c7127
toshiki <toshiki@Archlinux.localdomain>
2019-07-15 22:12:38 +0900

    WIP シングルトン化

--- src/model/logic/Game.java
+++ src/model/logic/Game.java
@@ -1,13 +1,18 @@
 
 package model.logic;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import model.dto.Board;
+import model.dto.History;
+import model.player.DarkPlayer;
 import model.player.Player;
+import model.player.WhitePlayer;
 
 public final class Game {
+  private static Game instance;
   private static final Map<Integer, Integer> DIRECTION =
       Collections.unmodifiableMap(new HashMap<Integer, Integer>() {
         private static final long serialVersionUID = 6067989766198502055L;
@@ -22,8 +27,35 @@ public final class Game {
           put(1, -1);
         }
       });
+  
+  private ArrayList<History> histories;
+  private Board board;
+  private Player player;
+  
+  
+  private Game() {
+    histories = new ArrayList<>();
+    board = new Board();
+    board.putStoneTo(3, 3, WhitePlayer.getInstance());
+    board.putStoneTo(3, 4, DarkPlayer.getInstance());
+    board.putStoneTo(4, 3, DarkPlayer.getInstance());
+    board.putStoneTo(4, 4, WhitePlayer.getInstance());
+    player = DarkPlayer.getInstance();
+  }
+  
+  public static Game getInstance() {
+    if (instance == null) {
+      instance = new Game();
+    }
+    return instance;
+  }
+  
+  public Player test(int x, int y) {
+    
+    return board.getPlayerOf(x, y);
+  }
 
-  public static Boolean putStoneIfTurnedOver(int x, int y, Player player, Board board) {
+  public Boolean putStoneIfTurnedOver(int x, int y, Player player) {
     if (!board.isPutableStone(x, y)) {
       return false;
     }
@@ -38,7 +70,7 @@ public final class Game {
       Player current = board.getPlayerOf(addedX, addedY);
 
       if (current != null && current != player) {
-        if (RecursivelyTurnOver(addedX, addedY, addX, addY, player, board)) {
+        if (RecursivelyTurnOver(addedX, addedY, addX, addY)) {
           result = true;
         }
       }
@@ -51,20 +83,18 @@ public final class Game {
     return result;
   }
 
-  private static Boolean RecursivelyTurnOver(int x, int y, int addX, int addY, Player player,
-      Board board) {
+  private Boolean RecursivelyTurnOver(int x, int y, int addX, int addY) {
     if (board.getPlayerOf(x, y) == null) {
       return false;
     }
     if (board.getPlayerOf(x, y) == player) {
       return true;
     }
-    if (RecursivelyTurnOver(x + addX, y + addY, addX, addY, player, board)) {
+    if (RecursivelyTurnOver(x + addX, y + addY, addX, addY)) {
       board.turnOver(x, y);
       return true;
     } else {
       return false;
     }
   }
-
 }
