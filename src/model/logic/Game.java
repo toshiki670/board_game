e0df4e81a22b9a89278e510f800e21ed3c6815ca
toshiki <toshiki@Archlinux.localdomain>
2019-07-15 23:55:02 +0900

    Add logic

--- src/model/logic/Game.java
+++ src/model/logic/Game.java
@@ -1,9 +1,9 @@
 
 package model.logic;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import model.dto.Board;
 import model.dto.History;
@@ -27,36 +27,59 @@ public final class Game {
           put(1, -1);
         }
       });
-  
-  private ArrayList<History> histories;
+
+  private LinkedList<History> histories;
   private Board board;
   private Player player;
-  
-  
+  private Boolean isPutStone;
+
+
   private Game() {
-    histories = new ArrayList<>();
+    histories = new LinkedList<>();
     board = new Board();
     board.putStoneTo(3, 3, WhitePlayer.getInstance());
     board.putStoneTo(3, 4, DarkPlayer.getInstance());
     board.putStoneTo(4, 3, DarkPlayer.getInstance());
     board.putStoneTo(4, 4, WhitePlayer.getInstance());
     player = DarkPlayer.getInstance();
+    histories.add(new History(player, board));
+    isPutStone = false;
   }
-  
+
   public static Game getInstance() {
     if (instance == null) {
       instance = new Game();
     }
     return instance;
   }
-  
-  public Player test(int x, int y) {
-    
-    return board.getPlayerOf(x, y);
+
+  public Player getPlayer() {
+    return player;
   }
 
-  public Boolean putStoneIfTurnedOver(int x, int y, Player player) {
-    if (!board.isPutableStone(x, y)) {
+  public Boolean commit() {
+    if (!isPutStone) {
+      return false;
+    }
+    player = Player.changer(player);
+    histories.add(new History(player, board));
+    isPutStone = false;
+    return true;
+  }
+
+  public Boolean fallback() {
+    if (!isPutStone) {
+      return false;
+    }
+    History h = histories.getLast();
+    player = h.getPlayer();
+    board = h.getBoard();
+    isPutStone = false;
+    return true;
+  }
+
+  public Boolean putStoneIfTurnedOver(int x, int y) {
+    if (isPutStone || !board.isPutableStone(x, y)) {
       return false;
     }
     Boolean result = false;
@@ -78,6 +101,7 @@ public final class Game {
 
     if (result) {
       board.putStoneTo(x, y, player);
+      isPutStone = true;
     }
 
     return result;
