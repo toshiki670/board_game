820605d922513018621ef6e4a4d6326c5329bc8b
toshiki <toshiki@Archlinux.localdomain>
2019-07-20 21:34:17 +0900

    Add 共通ヒストリーの追加

---
+++ src/model/base/dto/History.java
@@ -0,0 +1,30 @@
+/**
+ * (C) 2020 Toshiki.
+ */
+package model.base.dto;
+
+/**
+ * ボードの情報を履歴として扱うDTO.
+ * <p>
+ * BaseではBoardだけを扱っているため、継承して扱う
+ * 
+ * @author toshiki
+ * @since 1.0
+ */
+public final class History<T extends Cell> implements Cloneable {
+  private Board<T> board;
+
+  protected History(Board<T> board) {
+    this.board = board.clone();
+  }
+
+
+  protected Board<T> getBoard() {
+    return board.clone();
+  }
+
+  @Override
+  protected History<T> clone() throws CloneNotSupportedException {
+    return new History<T>(board.clone());
+  }
+}
