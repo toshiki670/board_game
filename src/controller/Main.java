541cab276c4e515bba5bf59a32101b70534a6c30
toshiki <toshiki@Archlinux.localdomain>
2019-07-21 19:22:28 +0900

    Create Othelloを試験するドライバ作成

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,10 +1,15 @@
 package controller;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import model.base.dto.Coord;
+import model.othello.logic.OthelloGame;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws NumberFormatException, IOException {
     // TODO Auto-generated method stub
 
     // show board
@@ -22,6 +27,30 @@ public class Main {
     // put a 1
     // put 1 A
     // put 1 a
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    
+    OthelloGame game = OthelloGame.getInstance();
+
+    if (!game.isEnd()) {
+      
+      game.getBoard().getField().forEach(System.out::println);
+      
+      int x = Integer.parseInt(br.readLine());
+      int y = Integer.parseInt(br.readLine());
+
+      
+      game.putStone(new Coord(x, y));
+
+      if (game.isStateOfPutPlace()) {
+        game.commit();
+
+      }
+
+    }
+
+
 
   }
 
