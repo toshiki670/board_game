205667751108327fa8411b5f6444a6f715b7fde2
toshiki <toshiki@Archlinux.localdomain>
2019-07-22 07:29:08 +0900

    Add 画面描写用のメソッドを追加

---
+++ src/view/render/dto/CellCharHashMap.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package view.render.dto;
+
+import java.util.HashMap;
+import model.base.dto.State;
+
+/**
+ * 状態と画面に描写する文字の対応DTO.
+ * 
+ * @author toshiki
+ * @since 1.0
+ */
+public class CellCharHashMap extends HashMap<State, String> {
+
+  /**
+   *  このクラスを修正するたびに更新すること
+   */
+  private static final long serialVersionUID = 2019072100L;
+}
